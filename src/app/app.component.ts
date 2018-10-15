6e49260e9194121316ce79eda4cef91e33e7d7e2
Med <mehdi.gacem@versusmind.eu>
2018-10-15 11:59:03 +0200

    Application component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tp2-garage';
+  title = 'TP2-Garage';
+  isEnable : boolean;
+  names : string[];
+
+  constructor()
+  {
+    this.isEnable = false;
+    this.names = ["Lenny", "Mehdi"];
+  }
+
+  toggle()
+  {
+    this.isEnable = !this.isEnable;
+  }
 }
